c942184ab75faeed74ed7c4e6fde09a2ca50e754
yasminechb <chebbiyasmine9@gmail.com>
2022-12-20 22:04:18 +0100

    cover-acc

--- frontuser/src/routes/Accueil.js
+++ frontuser/src/routes/Accueil.js
@@ -8,7 +8,7 @@ return(
         
       <Hero
       cName="hero"
-      heroImg="https://images.pexels.com/photos/1666816/pexels-photo-1666816.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+      heroImg="https://images.pexels.com/photos/1190298/pexels-photo-1190298.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
       title= "Vos meilleurs evenement"
       texte="Choisissez votre evenement préféré."
       buttonText="Consulter maintenant"
