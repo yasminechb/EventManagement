b05af37b522fde9ea26812ee9b3607711c05fa25
yasminechb <chebbiyasmine9@gmail.com>
2022-12-20 21:18:30 +0100

    sign-in-css2

--- FrontAdmin/src/pages/SignIn.js
+++ FrontAdmin/src/pages/SignIn.js
@@ -2,39 +2,44 @@ import React from 'react'
 import './SignIn.css';
 
 
-function SignIn() {
+const SignIn = () => {
   return (
-
-              <form >
-                <h2>Sign Up!</h2>
-                <fieldset>
-                  <legend>Create Account</legend>
-                  <ul className='lul'>
-                    <li>
-                      <label for="username">Username:</label>
-                      <input type="text" id="username" required/>
-                    </li>
-                    <li>
-                      <label for="email">Email:</label>
-                      <input type="email" id="email" required/>
-                    </li>
-                    <li>
-                      <label for="password">Password:</label>
-                      <input type="password" id="password" required/>
-                    </li>
-                  </ul>
-                </fieldset>
-                <button>Submit</button>
-                <button type="button" onClick={ () => this.changeView("logIn")}>Have an Account?</button>
-              </form>
-            )
+ 
+        <>
+           <div className="formulaire"/>
+        <form>
+    <h2>Sign Up!</h2>
+      <fieldset>
+        <legend>Create Account</legend>
+        <ul className='lul'>
+          <li>
+            <label for="username">Username:</label>
+            <input type="text" id="username" required/>
+          </li>
+          <li>
+            <label for="email">Email:</label>
+            <input type="email" id="email" required/>
+          </li>
+          <li>
+            <label for="password">Password:</label>
+            <input type="password" id="password" required/>
+          </li>
+        </ul>
+      </fieldset>
+      <button>Submit</button>
+      <button type="button" onClick={ () => this.changeView("logIn")}>Have an Account?</button>
       
+      
+      
+      
+      
+      
+      </form></>
+           );
+    
+            
       }
     
     
-      
-    
-    
+      export default SignIn ;
 
-
-export default SignIn
